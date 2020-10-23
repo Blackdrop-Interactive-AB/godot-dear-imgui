e7d33a18b52384b3384547107558f876d7c4d86c
Carl Söyseth <carl.soyseth@octacon.se>
2020-10-23 13:19:29 +0200

    Hello ImGui

--- native_imgui.cpp
+++ native_imgui.cpp
@@ -33,7 +33,7 @@ void native_imgui::_bind_methods() {
 
 void native_imgui::_process(float delta, RID parent) {
 	bool truebool = true;
-
+	
 	ImGui::NewFrame();
 	ImGui::Begin("Test");
 	ImGui::Text("ABOUT THIS DEMO:");
@@ -56,169 +56,93 @@ void native_imgui::_process(float delta, RID parent) {
 	ImGui::End();
 	ImGui::EndFrame(); 
 
-	ImGui::Render();
-	ImDrawData *draw_data = ImGui::GetDrawData();
+	ImGui::Render(); 
+	
+	Render(delta, nullptr, get_canvas_item());
+}
 
-	Render(delta, draw_data, get_canvas_item());
+
+
+Vector<Array> native_imgui::extract_imgui_data() {
+	ImDrawData *draw_dat = ImGui::GetDrawData();
+
+	// validate data
+	if (!draw_dat->Valid) print_error("IMGUI: DRAW DATA NOT VALID");
+
+	
+	Vector<Array> arrays;
+	// How many meshes
+	for (uint32_t i = 0; i < draw_dat->CmdListsCount; i++) {
+		ImDrawList * list = draw_dat->CmdLists[i];
+
+		Array temp;
+		temp.resize(ArrayMesh::ARRAY_MAX);
+	
+
+		Vector<Vector2> vertices;
+		Vector<Color> colors;
+		Vector<Vector2> uvs;
+		Vector<int> indices;
+
+		for (uint32_t j = 0; j < list->VtxBuffer.size(); j++) {
+			// vertex pos
+			ImVec2 im_vert = list->VtxBuffer[j].pos;
+			Vector2 godot_vert(im_vert.x, im_vert.y);
+
+			// vertex colour
+			ImU32 im_col = list->VtxBuffer[j].col;
+			uint8_t R = (im_col & 0xFF) / 255;
+			uint8_t G = ((im_col >> 8) & 0xFF) / 255;
+			uint8_t B = ((im_col >> 16) & 0xFF) / 255;
+			uint8_t A = ((im_col >> 24) & 0xFF) / 255;
+			Color godot_col(R, G, B, A);
+			// Vertex uv
+			ImVec2 im_uv = list->VtxBuffer[j].uv;
+			Vector2 godot_uv(im_uv.x, im_uv.y);
+
+			uvs.push_back(godot_uv);
+			vertices.push_back(godot_vert);
+			colors.push_back(godot_col);
+		}
+
+		for (uint32_t j = 0; j < list->IdxBuffer.size(); j++) {
+			indices.push_back(list->IdxBuffer[j]);
+		}
+
+
+		temp[(int)ArrayMesh::ArrayType::ARRAY_VERTEX] = vertices;
+		temp[(int)ArrayMesh::ArrayType::ARRAY_INDEX] = indices;
+		temp[(int)ArrayMesh::ArrayType::ARRAY_COLOR] = colors;
+		temp[(int)ArrayMesh::ArrayType::ARRAY_TEX_UV] = uvs;
+
+		arrays.push_back(temp);
+
+	}
+
+	
+
+
+	return arrays;
 }
 
 void native_imgui::Render(float delta, ImDrawData *draw_data, RID parent) {
 
+	Vector<Array> arrays = extract_imgui_data();
+	for (uint32_t i = 0; i < arrays.size(); i++) {
+		Vector<Color> temp = arrays[i][ArrayMesh::ARRAY_COLOR];
+		mesh.add_surface_from_arrays(Mesh::PrimitiveType::PRIMITIVE_TRIANGLES, arrays[i]);
 
 
-	Vector<Vector2> vertices;
-	Vector<Color> colors;
-	Vector<Vector2> uvs;
-	Vector<int> indices;
+		VisualServer->canvas_item_clear(get_canvas_item());
+		VisualServer->canvas_item_set_clip(get_canvas_item(), true); 
 
-	int factor = 100;
+		VisualServer->canvas_item_add_mesh(get_canvas_item(), mesh.get_rid(), Transform2D(), Color(), imgtex.get_rid());
 
-	// First face
-	vertices.push_back(Vector2(0, 0) * factor);
-	vertices.push_back(Vector2(1, 0) * factor);
-	vertices.push_back(Vector2(1, 1) * factor);
-
-	// Second face	
-	vertices.push_back(Vector2(1, 1) * factor);
-	vertices.push_back(Vector2(0, 1) * factor);
-	vertices.push_back(Vector2(0, 0) * factor);
-
-	
-
-	for (uint32_t i = 0; i < vertices.size(); i++)
-		indices.push_back(i);
-	
-	VisualServer->canvas_item_set_parent(newChild, parent);
-	VisualServer->canvas_item_set_draw_index(newChild, 0);
-
-
-
-	arrays.resize(ArrayMesh::ARRAY_MAX);
-	arrays[(int)ArrayMesh::ArrayType::ARRAY_VERTEX] = vertices;
-	arrays[(int)ArrayMesh::ArrayType::ARRAY_INDEX] = indices;
-	 
-	mesh.add_surface_from_arrays(Mesh::PrimitiveType::PRIMITIVE_TRIANGLES, arrays);
-	 
-;
-
-	//VisualServer->canvas_item_clear(newChild);
-	VisualServer->canvas_item_set_clip(newChild, true); 
-
-	VisualServer->canvas_item_add_mesh(newChild, mesh.get_rid(), Transform2D(), Color(1.0, 1.0, 1.0, 1.0));
-
-	update();
-
-	/*
-	// Let's process the data
-	uint32_t neededNodes = 0;
-	for (uint32_t i = 0; i < draw_data->CmdListsCount; i++) {
-		neededNodes += draw_data->CmdLists[i]->CmdBuffer.Size;
 	}
- 
-	while (children.size() < neededNodes) {
-		RID newChild = VisualServer->canvas_item_create();
-		VisualServer->canvas_item_set_parent(newChild, parent);
-		VisualServer->canvas_item_set_draw_index(newChild, children.size());
-		
-		children.push_back(newChild);
-		meshes.push_front(new ArrayMesh());
-	}
-
-	draw_data->ScaleClipRects(ImGui::GetIO().DisplayFramebufferScale);
- 
-
-	for (uint32_t n = 0, nodeN = 0; n < draw_data->CmdListsCount; n++, nodeN++) {
-		ImDrawList *cmdList = draw_data->CmdLists[n];
-		int idxOffset = 0;
-
-		int nVert = cmdList->VtxBuffer.Size;
-
-		Vector<Vector2> vertices; 
-		Vector<Color> colors;
-		Vector<Vector2> uvs;
-
-		// Build the vertexes from ImGui
-		for (int i = 0; i < cmdList->VtxBuffer.Size; i++) {
-			auto v = cmdList->VtxBuffer[i];
-			vertices.push_back(Vector2(v.pos.x, v.pos.y));
-			// need to reverse the color bytes
-			unsigned int c0 = 255;
-			(v.col & 0xFF);
-			unsigned int c1 = 255;
-			((v.col >> 8) & 0xFF);
-			unsigned int c2 = 255;
-			((v.col >> 16) & 0xFF);
-			unsigned int c3 = 255;
-			((v.col >> 24) & 0xFF);
-			colors.push_back(Color(c0, c1, c2, c3));
-
-			print_line(vformat("x %d. y %d w %d", v.uv.x, v.uv.y, c3));
-			uvs.push_back(Vector2(1, v.uv.y));
-		}
-	
-		for (int cmdi = 0; cmdi < cmdList->CmdBuffer.Size; cmdi++) {
-			ImDrawCmd drawCmd = cmdList->CmdBuffer[cmdi];
-
-			Vector<int> indices;
-
-			for (int i = idxOffset, j = 0; i < idxOffset + drawCmd.ElemCount; i++, j++) {
-				indices.push_back(cmdList->IdxBuffer[i]);
-			}
-
-			auto arrays = Array();
-
-			arrays.resize(ArrayMesh::ARRAY_MAX);
-			arrays[(int)ArrayMesh::ArrayType::ARRAY_VERTEX] = vertices;
-			arrays[(int)ArrayMesh::ArrayType::ARRAY_COLOR] = colors;
-			arrays[(int)ArrayMesh::ArrayType::ARRAY_TEX_UV] = uvs;
-			arrays[(int)ArrayMesh::ArrayType::ARRAY_INDEX] = indices;
-	
-
-			
-			ArrayMesh mesh = *meshes[nodeN];
-			mesh.clear_surfaces();
-		
-			mesh.add_surface_from_arrays(Mesh::PrimitiveType::PRIMITIVE_TRIANGLES, arrays);
-			
-			RID child = children[nodeN];
-			  
-			VisualServer->canvas_item_clear(child);
-			VisualServer->canvas_item_set_clip(child, true);
-			VisualServer->canvas_item_set_custom_rect(child, true, Rect2(drawCmd.ClipRect.x, drawCmd.ClipRect.y, drawCmd.ClipRect.z - drawCmd.ClipRect.x, drawCmd.ClipRect.w - drawCmd.ClipRect.y));
-			
-			VisualServer->canvas_item_add_mesh(child, mesh.get_rid(), get_transform(), Color(1.0, 1.0, 1.0), VisualServer->get_test_texture());
-	
-		}
-	}
-	*/
 }
 
 void native_imgui::RebuildFontAtlas() {
-	/*
-	ImGuiIO &io = ImGui::GetIO();
-	int width, height, bytesPerPixel;
-	uint8_t * pixels = nullptr;
-	//PackedByteArray p_data;
 
-
-	io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height, &bytesPerPixel);
-	p_data.resize(width * height * bytesPerPixel);
-
-
-	for (uint32_t i = 0; i < width * height * bytesPerPixel; i++) {
-		p_data.set(i, pixels[i]);
-	}
-
-	Image img = Image(width, height, false, Image::Format::FORMAT_RGBA8, p_data);
-
- 
-
-	
-	imgtex.create_from_image(img.duplicate());
-	
-	io.Fonts->SetTexID(ImTextureID(100));
-	io.Fonts->ClearTexData();
-	*/
 
 }
 
@@ -227,25 +151,41 @@ native_imgui::native_imgui() {
 	this->VisualServer = VisualServer::get_singleton();
  
 
-	print_line("Setting up IMGUI");	
+	print_line("Setting up IMGUI");
+
+	newChild = VisualServer->canvas_item_create();
+
+	
 	context = ImGui::CreateContext();
+	ImGuiIO &io = ImGui::GetIO();
 	ImGui::SetCurrentContext(context);
 
-	ImGuiIO &io = ImGui::GetIO();
 	io.Fonts->AddFontDefault();
 
 	
-	newChild = VisualServer->canvas_item_create();
 
-	int width, height;
+	int width, height, bytesPerPixel;
 	unsigned char *pixels = NULL;
-	io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
+	io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height, &bytesPerPixel);
+
+
+	PoolByteArray test;
+
+	for (uint32_t i = 0; i < width * height * bytesPerPixel; i++) {
+		test.push_back(pixels[i]);
+	}
+
+	Image img(width, height, false, Image::Format::FORMAT_RGBA8, test); 
+
+	imgtex.create_from_image(img.duplicate(), 0);
+
+	io.Fonts->TexID = ImTextureID(100);
+
+	io.Fonts->ClearTexData();
+
 
 	io.DeltaTime = 1.0 / 60.0;
-	 get_process_delta_time();
 	io.DisplaySize.x = 	GLOBAL_GET("display/window/size/width"); // set the current display width
 	io.DisplaySize.y = GLOBAL_GET("display/window/size/height");
-
-	RebuildFontAtlas();
 
 }
